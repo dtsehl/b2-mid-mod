59b7604630aff478394635afea4b71a4b3990a78
Dan Sehl <dan.sehl@gmail.com>
2020-07-17 11:17:51 -0600

    Add mechanics_rides #create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/mechanics', to: 'mechanics#index'
+  get '/mechanics/:id', to: 'mechanics#show'
+
+  post '/mechanics/:id/rides', to: 'mechanic_rides#create'
 
   get '/amusement_parks/:id', to: 'amusement_parks#show'
 end
